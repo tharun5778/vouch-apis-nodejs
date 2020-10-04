a504967e2b588af5261df0727e5ac4065cc3a579
tharun5778 <57627667+tharun5778@users.noreply.github.com>
2020-10-05 01:05:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -97,7 +97,6 @@ app.get('/auth_verify/:oauth_token/:oauth_verifier', (req, res) => {
 
 })
 app.get("/error", (req, res) => {
-        console.log("error", sssssss)
         throw Error('error in code');
 
     })
