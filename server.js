664934914838b2aa92a4db3c373217f09ee42cac
tharun5778 <57627667+tharun5778@users.noreply.github.com>
2020-10-04 23:48:10 +0530

    nohup added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+throw ("hi")
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "*"); //
