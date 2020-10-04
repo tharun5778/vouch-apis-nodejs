c9611c546fbdcd1f57e2682dec664f88b6eebc06
tharun5778 <57627667+tharun5778@users.noreply.github.com>
2020-10-04 20:33:41 +0530

    moment not required so removed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const axios = require('axios')
-const moment = require('moment')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser');
